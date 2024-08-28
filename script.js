a08294985d835996660c87de2e1aa242ba82443b
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-28 16:33:53 +0200

    submit

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 let button = document.getElementById("new-task");
 let body = document.querySelector("body");
-let form = document.querySelector("form");
+let ul = document.querySelector("ul");
 
 let createForm = function () {
   let form = document.createElement("form");
+
   let close = document.createElement("div");
   close.innerHTML = "<i class='fas fa-window-close'></i>";
   close.id = "close";
   close.onclick = function () {
     form.remove();
   };
+
   let labelCalendar = document.createElement("label");
-  labelCalendar.for = "calendar";
+  labelCalendar.setAttribute("for", "calendar");
   let calendar = document.createElement("input");
   calendar.type = "date";
   calendar.id = "calendar";
+
   let textLabel = document.createElement("label");
-  textLabel.for = "todo";
+  textLabel.setAttribute("for", "todo");
   let textArea = document.createElement("textarea");
   textArea.id = "todo";
   textArea.rows = "4";
   textArea.cols = "50";
   textArea.placeholder = "Insert task here";
+
   let submit = document.createElement("button");
   submit.type = "submit";
   submit.id = "submit";
   submit.innerText = "submit!";
-  body.appendChild(form);
+
   form.append(close, labelCalendar, calendar, textLabel, textArea, submit);
+  body.appendChild(form);
+
+  // qui finisco di creare il form
+
+  // qui inizio il processo per ottenere i dati
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // evita il refresh della pagina
+
+    // posizionandomi qui ottengo i dati dopo che l'utente ha inserito submit
+    let taskText = textArea.value;
+    let taskDate = calendar.value;
+
+    // qui genero un nuovo LI da inserire dentro UL
+    let taskItem = document.createElement("li");
+    taskItem.innerText = `${taskDate}: ${taskText}`;
+    ul.appendChild(taskItem);
+
+    // finito tutto si autorimuove
+    form.remove();
+  });
 };
 
 button.addEventListener("click", createForm);
+
+let checked = function (event) {
+  let li = document.querySelector;
+};
